7c5289c203aac5353e2864b1faf40e0a8ae842e1
ocamilomontealegre <ocamilomontealegre@gmail.com>
2024-09-21 19:55:46 -0500

    chore: update logger config

--- src/common/logger/logger.config.ts
+++ src/common/logger/logger.config.ts
@@ -11,46 +11,55 @@ const { colorize, combine, json, label, printf, timestamp } = format;
 
 @injectable()
 export class Logger {
-  private readonly logger: WinstonLogger;
+  private readonly _loggerLabel!: string;
+  private _logger?: WinstonLogger;
 
-  public constructor(private readonly _loggerLabel: string = "ExpressApplication") {
-    this.logger = createLogger({
-      level: "debug",
-      format: combine(
-        label({ label: this._loggerLabel }),
-        timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
-        printf(
-          (info) =>
-            `\x1B[33m\x1B[3[${info.label}\x1B[23m\x1B[39m \x1B[32m${info.timestamp}\x1B[39m ${info.level}: ${info.message}`,
+  public constructor(_loggerLabel: string = "ExpressApplication") {
+    this._loggerLabel = _loggerLabel;
+  }
+
+  private readonly consoleTransport = new transports.Console({
+    format: combine(
+      colorize({
+        all: true,
+        colors: {
+          info: "bold blue",
+          warn: "bold yellow",
+          error: "bold red",
+          debug: "bold magenta",
+        },
+      }),
+    ),
+  });
+
+  private readonly debugFileTransport = new transports.File({
+    filename: "logs/debug.log",
+    format: combine(json()),
+  });
+
+  private readonly exceptionFileTransport = new transports.File({
+    filename: "logs/exceptions.log",
+    format: combine(json()),
+  });
+
+  private get logger(): WinstonLogger {
+    if (!this._logger) {
+      this._logger = createLogger({
+        level: "debug",
+        format: combine(
+          label({ label: `[${this._loggerLabel}]` }),
+          timestamp({
+            format: "MMM-DD-YYYY HH:mm:ss",
+          }),
+          printf(function (info) {
+            return `\x1B[33m\x1B[3m${info.label}\x1B[23m\x1B[39m \x1B[32m${info.timestamp}\x1B[39m ${info.level} : ${info.message}`;
+          }),
         ),
-      ),
-      transports: [
-        new transports.Console({
-          format: combine(
-            colorize({
-              all: true,
-              colors: {
-                info: "bold blue",
-                warn: "bold yellow",
-                error: "bold red",
-                debug: "bold magenta",
-              },
-            }),
-          ),
-        }),
-        new transports.File({
-          filename: "logs/debug.log",
-          format: combine(json()),
-        }),
-      ],
-      exceptionHandlers: [
-        new transports.Console(),
-        new transports.File({
-          filename: "logs/exceptions.log",
-          format: combine(json()),
-        }),
-      ],
-    });
+        transports: [this.consoleTransport, this.debugFileTransport],
+        exceptionHandlers: [this.consoleTransport, this.exceptionFileTransport],
+      });
+    }
+    return this._logger;
   }
 
   public info(message: string, callback?: LogCallback): void {
